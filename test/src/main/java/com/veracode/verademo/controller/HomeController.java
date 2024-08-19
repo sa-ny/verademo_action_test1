18414732ce18f438a50722c2dfe70b7747f64af8
Sany Anton Cyrus <sanycyrus@gmail.com>
2024-08-19 11:48:37 +0530

    new actions test

--- test/src/main/java/com/veracode/verademo/controller/HomeController.java
+++ test/src/main/java/com/veracode/verademo/controller/HomeController.java
@@ -3,12 +3,12 @@ package com.veracode.verademo.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.veracode.verademo.utils.Utils;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.veracode.verademo.utils.Utils;
 
 @Controller
 public class HomeController {
@@ -16,7 +16,7 @@ public class HomeController {
 	public String renderGet(Model model, HttpServletRequest request, HttpServletResponse response) {
 		// Check if user is already logged in.sa,
 		if (Utils.getSessionUserName(request, response) != null) {
-			// Redirect to user's feed
+			// Redirect to user's feed..
 			return Utils.redirect("feed");
 		}
 
