f9569688770e613430fae7454dfbfab875272914
sany cyrus <sanycyrus@gmail.com>
2024-09-23 18:49:30 +0530

    Update IgnoreCommand.java

--- test/src/main/java/com/veracode/verademo/commands/IgnoreCommand.java
+++ test/src/main/java/com/veracode/verademo/commands/IgnoreCommand.java
@@ -5,7 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-
+//..
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
