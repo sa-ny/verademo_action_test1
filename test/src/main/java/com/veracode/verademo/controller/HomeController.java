fabeaa296a7b9fb4f2d0b1a8ebee7ed68ab45d57
sany cyrus <sanycyrus@gmail.com>
2024-09-23 14:03:13 +0530

    Update HomeController.java

--- test/src/main/java/com/veracode/verademo/controller/HomeController.java
+++ test/src/main/java/com/veracode/verademo/controller/HomeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.veracode.verademo.utils.Utils;
-
+// trigger for annotation test
 @Controller
 public class HomeController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
