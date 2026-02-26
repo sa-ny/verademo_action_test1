c3b27b6f4ad9df08e33c21bd249672255ec44869
Sany Anton Cyrus <sanycyrus@gmail.com>
2026-02-26 19:43:07 +0530

    test all

--- test/src/main/java/com/veracode/verademo/controller/UserController.java
+++ test/src/main/java/com/veracode/verademo/controller/UserController.java
@@ -44,7 +44,7 @@ public class UserController {
 		}
 		//..
 
-		logger.info("Entering showLogin with username " + username + " and target " + target);
+		logger.info("Entering showLogin with username " + username + " and target.. " + target);
 
 		model.addAttribute("username", username);
 		model.addAttribute("target", target);
